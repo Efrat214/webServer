812695ee044bba4977929af921c4fcceeff3faa5
Efrat <efrat71499@gmail.com>
2023-11-05 13:44:33 +0200

    add other routes

--- expressServer.js
+++ expressServer.js
@@ -1,14 +1,46 @@
 import express from "express";
-import { generatePrime } from "./prime.js";
+import { generatePrime, isPrime } from "./prime.js";
 import * as dotenv from "dotenv";
 import env from "env-var";
 dotenv.config({ path: ".env" });
 
 const app = express();
 
+app.post("/api/numbers/prime/validate", (req, res) => {
+  try {
+    const body = req.body;
+    const prime = true;
+    for (const key in body) {
+      if (Object.hasOwnProperty.call(body, key)) {
+        const value = body[key];
+        if (!isPrime(value)) {
+          prime = false;
+        }
+      }
+    }
+    if (prime) {
+      res.status(200).send("true");
+    } else {
+      res.status(200).send("false");
+    }
+  } catch (err) {
+    res.status(400).send("Invalid JSON data");
+  }
+});
+
+app.get("/api/numbers/prime", (req, res) => {
+  const { amount } = req.query;
+  if (isNaN(amount)) {
+    res.status(400).send("Invalid 'amount' query parameter");
+  } else {
+    const nPrimeNumbers = generatePrime(amount);
+    res.status(200).send(nPrimeNumbers);
+  }
+});
+
 app.set("view engine", "pug");
 
-app.get("/API/numbers/prime/display", (req, res) => {
+app.get("/api/numbers/prime/display", (req, res) => {
   res.render("index", { primeNumbers: generatePrime(10) });
 });
 
